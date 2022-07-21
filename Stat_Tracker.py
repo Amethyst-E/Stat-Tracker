a28f58def3e6bd096e4f7210797295e005b7152b
Ame <97403821+Amethyst-E@users.noreply.github.com>
2022-07-20 21:19:37 -0400

    file selection
    Added the ability to select from multiple data sets, stored as individual txt files, upon startup.

--- Stat_Tracker.py
+++ Stat_Tracker.py
@@ -1,11 +1,32 @@
 # %%
+import glob
 from pynput import keyboard
 
+# file selection
+files = glob.glob("Stats/*.txt")
+leave = "exit"
+while True:
+    filename = input("please select your variable set by inputing the file name without the file extension and hitting enter:")
+    fileN = str("Stats\\" + filename + ".txt")
+    print(fileN)
+    if fileN in files:
+        print("file " + filename + " found")
+        break
+    else:
+        if filename == leave:
+            print("quiting program")
+            quit()
+        else:
+            print("could not find " + filename)
+            print("please try again")
+
+# setting up variable list, reading variables from chosen file and assigning them to the "var" list
+
 var = [0,0,0,0,0,0,0,0,0,0]
-incr = [1]
+incr = [1] # incrementing value
 print(var)
 
-with open('test2.txt') as txt_file:
+with open(fileN) as txt_file:
     line_count = 0
     for row in txt_file:
         print(f'Var {line_count}: {", ".join(row)}')
@@ -14,13 +35,13 @@ with open('test2.txt') as txt_file:
     print(f'Processed {line_count} lines.')
 
 def writetxt(number,index):
-    lines = open('test2.txt', 'r').readlines()
+    lines = open(fileN, 'r').readlines()
     lines[index] = str(number)+'\n'
-    out = open('test2.txt', 'w')
+    out = open(fileN, 'w')
     out.writelines(lines)
     out.close()
 
-var[0] = 0
+var[0] = 0 #this sets the first 2 values in the list to 0 to serve as current instace of this stat tracker. The lifetime incrementation of these values is stored in var7 and var8 respectively.
 writetxt(var[0],0)
 var[1] = 0
 writetxt(var[1],1)
@@ -61,7 +82,7 @@ def on_release(key):
             writetxt(var[8],8)
             return True
         case keyboard.KeyCode(vk=98):
-            var[2] += incr[0]
+            var[2] += (incr[0] * 5)
             writetxt(var[2],2)
             return True
         case keyboard.KeyCode(vk=99):
@@ -80,14 +101,14 @@ def on_release(key):
             var[6] += incr[0]
             writetxt(var[6],6)
             return True
-        case keyboard.KeyCode(vk=103):
-            var[7] += incr[0]
-            writetxt(var[7],7)
-            return True
-        case keyboard.KeyCode(vk=104):
-            var[8] += incr[0]
-            writetxt(var[8],8)
-            return True
+       # case keyboard.KeyCode(vk=103):
+         #   var[7] += incr[0]
+         #   writetxt(var[7],7)
+         #   return True
+        #case keyboard.KeyCode(vk=104):
+         #   var[8] += incr[0]
+         #   writetxt(var[8],8)
+         #   return True
         case keyboard.KeyCode(vk=105):
             var[9] += incr[0]
             writetxt(var[9],9)
